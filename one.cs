e838ae9691564aa11577c6162721ae587659461f
vasuems <vasu.ems@gmail.com>
2022-12-23 23:03:40 +0530

    Adding New File

---
+++ one.cs
@@ -0,0 +1,15 @@
+#region imports
+using  com.framework.util;
+using  com.framework.security;
+#endregion
+
+namespace com.contoso.codegen
+{
+	#region class
+	public  class  One
+	{				
+		#region methods
+		/***  function1  ***/
+		#endregion			
+	}
+}
