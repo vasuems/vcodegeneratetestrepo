18cb20c64b3517b01788b59b1d94a35ead5849ea
vasuems <vasu.ems@gmail.com>
2022-12-23 23:00:41 +0530

    Adding New File

---
+++ two.cs
@@ -0,0 +1,15 @@
+#region imports
+using  com.framework.util;
+using  com.framework.security;
+#endregion
+
+namespace com.contoso.codegen
+{
+	#region class
+	public  class  Two
+	{				
+		#region methods
+		/***  function2  ***/
+		#endregion			
+	}
+}
